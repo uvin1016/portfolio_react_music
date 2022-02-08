8907901269541acfad6ed44447743f14695f35bd
uvin1016 <uvin1016@naver.com>
2022-02-08 21:25:32 +0900

    header 디자인 수정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,13 @@
 import {NavLink} from 'react-router-dom';
 
 function Header(){
+    let active = {color: "white"};
+
     return (
         <header>
             <div className="inner">
                 <h1><NavLink exact to="/">UFOLIO</NavLink></h1>
-                <NavLink to="/department">BOARD</NavLink>
+                <NavLink activeStyle={active} to="/department">BOARD</NavLink>
             </div>
         </header>
     )
